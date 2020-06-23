29bedbfbb528bea6f089dfc7742c552400b0508c
Jason Patrick <jason@glyphgames.com>
2020-06-23 16:15:13 +0800

    Added new forecast data

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@ const weatherForm = document.querySelector('form');
 const searchElement = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
+const messageThree = document.querySelector('#message-3');
 
 
 
@@ -9,16 +10,22 @@ const messageTwo = document.querySelector('#message-2');
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault();
     messageOne.textContent = 'Loading....';
-    messageTwo.textContent= '';
+    messageTwo.textContent = '';
+    messageThree.textContent = '';
     const location = searchElement.value; 
     fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
             if(!data.error) {
                 messageOne.textContent = data.location
-                messageTwo.textContent = data.forecast.rainChance + " Chance of Rain - " + "Weather Summary: "+ data.forecast.summary + " - Temperature is: " + data.forecast.temperature + " Degrees Celsius" 
+                messageTwo.textContent = data.forecast.rainChance + 
+                " Chance of Rain - " + "Weather Summary: " 
+                + data.forecast.summary + " - Temperature is: " + data.forecast.temperature
+                + " Degrees Celsius" ;
+                messageThree.textContent = "Timezone: " + data.forecast.timezone + " Local Time: " + data.forecast.localTime;
             } else {    
-               messageOne.textContent = ""
-               messageTwo.textContent = data.error
+               messageOne.textContent = "";
+               messageTwo.textContent = data.error;
+               messageThree.textContent = "";
             }
         })
     })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,22 +12,12 @@ const forecast = (address, callback) => {
                 temperature: body.current.temperature,
                 summary: body.current.weather_descriptions[0],
                 rainChance: body.current.precip * 100 + "%",
-                location: body.location.name + " " + body.location.country
+                location: body.location.name + " " + body.location.country,
+                localTime: body.location.localtime,
+                timezone: body.location.timezone_id
              })
         }
     })
 }
 
 module.exports = forecast
-
-
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
